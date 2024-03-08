14fc52c144df1f6a0d8efe01530ad73d5d5796eb
bianxin <961116028@qq.com>
2024-03-09 00:22:27 +0800

    添加了优惠券的代码结构-测试

--- sky-server/src/main/java/com/sky/service/impl/VoucherServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/VoucherServiceImpl.java
@@ -1,14 +1,20 @@
 package com.sky.service.impl;
 
 import com.sky.entity.Voucher;
+import com.sky.mapper.VoucherMapper;
 import com.sky.result.Result;
 import com.sky.service.VoucherService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class VoucherServiceImpl implements VoucherService {
+
+
+    @Autowired
+    private VoucherMapper voucherMapper;
 
     @Override
     public void addSeckillVoucher(Voucher voucher) {
